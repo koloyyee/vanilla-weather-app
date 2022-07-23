bf377490a2c03badfccbd85a84f70a4179bd938e
Loy Yee Ko <koloyyee@gmail.com>
2022-07-23 21:23:23 +0800

    commit before pushing to netlify.

---
+++ env.js
@@ -0,0 +1,5 @@
+const AccessKey = "aFOYzOBdTFaGYzD7plqjPHlDTnPGaz0cyYV5j4PRXWQ";
+const SecretKey = "ehL5NpKDFcGbCCl0DJDpmJ-41NeSou5lb6yVLw62PTI";
+const RapidAPIKey = "037aaf43c6msh6f0cec8a0dd8891p16c374jsn722e720a3103";
+
+export { AccessKey, SecretKey, RapidAPIKey };
